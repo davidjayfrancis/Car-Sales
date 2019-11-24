c77762c29c23c743317df73d9e7678d52b3d8530
David Francis <davidjayfrancis@gmail.com>
2019-11-23 21:56:43 -0800

    added logger middleware and a localstorage middleware to persist state :)

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import { logger } from "redux-logger";
+import persistState from "redux-localstorage";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
 import redux from "redux";
-import { createStore } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 
 //import reducers and actions
 import reducer from "./reducers/reducer.js";
 
-const store = createStore(reducer);
+const enhancer = compose(applyMiddleware(logger), persistState());
+
+const store = createStore(reducer, enhancer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
